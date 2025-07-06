3d3ad20f18512225965f02ab2bfc062fb84f7e13
Arnarit Bhunia <164724433+Arnarit@users.noreply.github.com>
2025-07-06 20:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
+
 import tempfile
 import time
 import io
